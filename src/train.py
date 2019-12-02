beba5552fae825a9321a823beda0b247c338ef7e
salmedina <salbed.medina@gmail.com>
2019-12-02 04:09:27 -0500

    nit pick argument order

--- src/train.py
+++ src/train.py
@@ -17,11 +17,12 @@ def parse_args():
     parser.add_argument('--lr', type=float, default=0.03, help='Training learning rate')
     parser.add_argument('--wd', type=float, default=1e-4, help='Training weight decay')
     parser.add_argument('--early_stop', action='store_true', default=False, help='Enable/Disable early stopping based on F1')
+    parser.add_argument('--valid_freq', type=int, help='Number of epochs when the validation will be run')
     parser.add_argument('--data_path', type=str, default='../data/labeled_data.json', help='Path to the json file to use for classification')
     parser.add_argument('--experiment_path', type=str, default='', help='Path to the experiment description yaml file')
-    parser.add_argument('--valid_freq', type=int, help='Number of epochs when the validation will be run')
     parser.add_argument('--output_path', type=str, default='./output',
                         help='Path to the directory where output will be saved')
+
     return parser.parse_args()
 
 
