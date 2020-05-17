59131d16b6997b33edd049cd714a6f51189bfabc
Salvador Medina <salvadom@andrew.cmu.edu>
2020-05-16 21:33:26 -0400

    Change arg number_layers -> num_layers

--- src/BiLSTM_Static.py
+++ src/BiLSTM_Static.py
@@ -7,13 +7,13 @@ from revgrad import RevGrad
 
 class BiLSTM_BERT(nn.Module):
 
-    def __init__(self, embedding_dim, hidden_dim, label_size, use_gpu, batch_size, number_layers, dropout=0.5):
+    def __init__(self, embedding_dim, hidden_dim, label_size, use_gpu, batch_size, num_layers, dropout=0.5):
         super(BiLSTM_BERT, self).__init__()
         self.hidden_dim = hidden_dim
         self.use_gpu = use_gpu
         self.batch_size = batch_size
         self.dropout = dropout
-        self.number_layers = number_layers
+        self.number_layers = num_layers
         self.label_size = label_size
         self.lstm = nn.LSTM(input_size=embedding_dim, hidden_size=hidden_dim, bidirectional=True, batch_first=True,
                             num_layers= self.number_layers, dropout= self.dropout)
